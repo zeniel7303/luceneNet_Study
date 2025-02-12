8512de09c480f70b0c8aa2343b6c04976a2c34b0
안도현 <ahnjm001@playwith.co.kr>
2025-02-12 18:55:54 +0900

    feat: 필드를 줄이는 방법 연구를 위한 V2 추가

--- lucenenet_study/MySearchV2.cs
+++ lucenenet_study/MySearchV2.cs
@@ -13,7 +13,7 @@ using LuceneDirectory = Lucene.Net.Store.Directory;
 
 namespace LucenenetStudy;
 
-public class MySearch
+public class MySearchV2
 {
     private const LuceneVersion luceneVersion = LuceneVersion.LUCENE_48;
 
@@ -23,7 +23,7 @@ public class MySearch
     
     private static readonly string[] fields = new[] { "name", "option1", "value1", "option2", "value2"};
 
-    public MySearch(string indexPath)
+    public MySearchV2(string indexPath)
     {
         _writer = new IndexWriter(FSDirectory.Open(indexPath),
             new IndexWriterConfig(luceneVersion, new StandardAnalyzer(luceneVersion))
@@ -36,6 +36,8 @@ public class MySearch
     
     public void Index()
     {
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        
         var items = new[]
         {
             new[] { "롱 소드", "힘 증가", "10", "물리 공격력 증가", "12" },
@@ -46,12 +48,12 @@ public class MySearch
 
         var counts = new int[items.Length];
         var random = new Random();
-        for (var i = 0; i < 10000; i++)
+        for (var i = 0; i < 100; i++)
         {
             var index = random.Next(items.Length);
             var item = items[index];
             counts[index]++;
-            AddDocument($"{i}", item[0], item[1], int.Parse(item[2]), item[3], int.Parse(item[4]));
+            AddDocument(item[0], item[1], int.Parse(item[2]), item[3], int.Parse(item[4]));
         }
 
         for (var i = 0; i < items.Length; i++)
@@ -62,48 +64,45 @@ public class MySearch
         _writer.Commit();
         
         Console.WriteLine($"세팅 종료 \n");
+        
+        stopwatch.Stop(); // 수행 시간 측정 종료
+        Console.WriteLine($"Search execution time: {stopwatch.ElapsedMilliseconds} ms \n");
     }
 
-    private void AddDocument(string id, string name, string option1, int value1, string option2, int value2)
+    private void AddDocument(string name, string option1, int value1, string option2, int value2)
     {
         var document = new Document
         {
-            new StringField("id", id, Field.Store.YES),
             new StringField("name", name.Replace(" ", ""), Field.Store.YES),
-            new StringField("option1", option1.Replace(" ", ""), Field.Store.YES),
-            new Int64Field("value1", value1, Field.Store.YES),
-            new StringField("option2", option2.Replace(" ", ""), Field.Store.YES),
-            new Int64Field("value2", value2, Field.Store.YES)
+            new StringField("option1_value1", $"{option1.Replace(" ", "")}:{value1}", Field.Store.YES),
+            new StringField("option2_value2", $"{option2.Replace(" ", "")}:{value2}", Field.Store.YES)
         };
         _writer.AddDocument(document);
     }
-
-    public void Search(string query)
+    
+    public void SearchWithName(string query)
     {
         Console.WriteLine($"검색어: {query}");
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-        
+    
         _searchManager.MaybeRefreshBlocking();
 
         var searcher = _searchManager.Acquire();
         try
         {
             var booleanQuery = new BooleanQuery();
-            
+        
             // 입력된 쿼리에서 띄어쓰기 제거
             var queryWithoutWhiteSpace = query.Replace(" ", "");
 
-            // fields 배열의 모든 필드에 대해 WildcardQuery 추가
-            foreach (var field in fields)
-            {
-                booleanQuery.Add(new WildcardQuery(new Term(field, $"*{queryWithoutWhiteSpace}*")), Occur.SHOULD);
-            }
+            // name 필드에 대해 WildcardQuery 추가
+            booleanQuery.Add(new WildcardQuery(new Term("name", $"*{queryWithoutWhiteSpace}*")), Occur.SHOULD);
 
             var topDocs = searcher.Search(booleanQuery, n: 10);
-            
+        
             Console.WriteLine($"검색 결과 수: {topDocs.TotalHits}");
             Console.WriteLine("===============================================================================");
-            
+
             foreach (var scoreDoc in topDocs.ScoreDocs)
             {
                 PrintDocumentFields(searcher, scoreDoc);
@@ -113,40 +112,76 @@ public class MySearch
         {
             _searchManager.Release(searcher);
             searcher = null;
-            
+        
+            stopwatch.Stop(); // 수행 시간 측정 종료
+            Console.WriteLine($"Search execution time: {stopwatch.ElapsedMilliseconds} ms \n");
+        }
+    }
+
+    public void SearchWithOption(string query)
+    {
+        Console.WriteLine($"검색어: {query}");
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+    
+        _searchManager.MaybeRefreshBlocking();
+
+        var searcher = _searchManager.Acquire();
+        try
+        {
+            var booleanQuery = new BooleanQuery();
+        
+            // 입력된 쿼리에서 띄어쓰기 제거
+            var queryWithoutWhiteSpace = query.Replace(" ", "");
+
+            // options 필드에 대해 WildcardQuery 추가
+            booleanQuery.Add(new WildcardQuery(new Term("option1_value1", $"*{queryWithoutWhiteSpace}*")), Occur.SHOULD);
+            booleanQuery.Add(new WildcardQuery(new Term("option2_value2", $"*{queryWithoutWhiteSpace}*")), Occur.SHOULD); // 수정된 부분
+
+            var topDocs = searcher.Search(booleanQuery, n: 10);
+        
+            Console.WriteLine($"검색 결과 수: {topDocs.TotalHits}");
+            Console.WriteLine("===============================================================================");
+
+            foreach (var scoreDoc in topDocs.ScoreDocs)
+            {
+                PrintDocumentFields(searcher, scoreDoc);
+            }
+        }
+        finally
+        {
+            _searchManager.Release(searcher);
+            searcher = null;
+        
             stopwatch.Stop(); // 수행 시간 측정 종료
             Console.WriteLine($"Search execution time: {stopwatch.ElapsedMilliseconds} ms \n");
         }
     }
     
-    public void Search(string query1, string query2)
+    public void SearchWithOptions(string query1, string query2)
     {
         Console.WriteLine($"검색어: {query1}, {query2}");
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-        
+    
         _searchManager.MaybeRefreshBlocking();
 
         var searcher = _searchManager.Acquire();
         try
         {
             var booleanQuery = new BooleanQuery();
-            
+        
             // 입력된 쿼리에서 띄어쓰기 제거
             var query1WithoutWhiteSpace = query1.Replace(" ", "");
             var query2WithoutWhiteSpace = query2.Replace(" ", "");
 
-            // fields 배열의 모든 필드에 대해 WildcardQuery 추가
-            foreach (var field in fields)
-            {
-                booleanQuery.Add(new WildcardQuery(new Term("option1", $"*{query1WithoutWhiteSpace}*")), Occur.MUST);
-                booleanQuery.Add(new WildcardQuery(new Term("option2", $"*{query2WithoutWhiteSpace}*")), Occur.MUST);
-            }
+            // options 필드에 대해 WildcardQuery 추가
+            booleanQuery.Add(new WildcardQuery(new Term("option1_value1", $"*{query1WithoutWhiteSpace}*")), Occur.MUST);
+            booleanQuery.Add(new WildcardQuery(new Term("option2_value2", $"*{query2WithoutWhiteSpace}*")), Occur.MUST);
 
             var topDocs = searcher.Search(booleanQuery, n: 10);
-            
+        
             Console.WriteLine($"Matching results: {topDocs.TotalHits}");
             Console.WriteLine("===============================================================================");
-            
+
             foreach (var scoreDoc in topDocs.ScoreDocs)
             {
                 PrintDocumentFields(searcher, scoreDoc);
@@ -156,17 +191,17 @@ public class MySearch
         {
             _searchManager.Release(searcher);
             searcher = null;
-            
+        
             stopwatch.Stop(); // 수행 시간 측정 종료
             Console.WriteLine($"Search execution time: {stopwatch.ElapsedMilliseconds} ms \n");
         }
     }
 
-    public void SearchByRange(int optionIndex, int minValue, int maxValue)
+    public void SearchByOptionAndRange(int n, string optionName, int minValue, int maxValue) // n번째 옵션 추가
     {
-        Console.WriteLine($"범위 검색: option{optionIndex} {minValue}~{maxValue}");
+        Console.WriteLine($"{n}번 옵션 범위 검색: {optionName} {minValue}~{maxValue}");
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-        
+    
         _searchManager.MaybeRefreshBlocking();
 
         var searcher = _searchManager.Acquire();
@@ -174,25 +209,18 @@ public class MySearch
         {
             var booleanQuery = new BooleanQuery();
             
-            // 옵션에 따라 검색할 필드 선택
-            var valueField = $"value{optionIndex}";
-
-            // 범위 쿼리 생성
-            var rangeQuery = NumericRangeQuery.NewInt64Range(
-                valueField,
-                minValue,
-                maxValue,
-                true,  // minInclusive
-                true   // maxInclusive
-            );
+            // 너무 무식한 방법이지만 일단 다르게 생각나는 방법이 없음
+            // minValue와 maxValue 사이의 값을 찾기 위한 쿼리 추가
+            for (var value = minValue; value <= maxValue; value++)
+            {
+                booleanQuery.Add(new WildcardQuery(new Term($"option{n}_value{n}", $"*{optionName.Replace(" ", "")}:{value}*")), Occur.SHOULD);
+            }
             
-            booleanQuery.Add(rangeQuery, Occur.MUST);
-
             var topDocs = searcher.Search(booleanQuery, n: 10);
-            
+        
             Console.WriteLine($"검색 결과 수: {topDocs.TotalHits}");
             Console.WriteLine("===============================================================================");
-            
+
             foreach (var scoreDoc in topDocs.ScoreDocs)
             {
                 PrintDocumentFields(searcher, scoreDoc);
@@ -202,60 +230,7 @@ public class MySearch
         {
             _searchManager.Release(searcher);
             searcher = null;
-            
-            stopwatch.Stop();
-            Console.WriteLine($"검색 소요 시간: {stopwatch.ElapsedMilliseconds} ms \n");
-        }
-    }
-
-    public void SearchByOptionAndRange(int optionIndex, string optionName, int minValue, int maxValue)
-    {
-        Console.WriteLine($"옵션 범위 검색: {optionName} {minValue}~{maxValue}");
-        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
         
-        _searchManager.MaybeRefreshBlocking();
-
-        var searcher = _searchManager.Acquire();
-        try
-        {
-            var booleanQuery = new BooleanQuery();
-            
-            // 옵션 필드와 값 필드 선택
-            var optionField = $"option{optionIndex}";
-            var valueField = $"value{optionIndex}";
-
-            // 옵션 이름 쿼리
-            var optionQuery = new WildcardQuery(
-                new Term(optionField, $"*{optionName.Replace(" ", "")}*")
-            );
-            
-            // 범위 쿼리
-            var rangeQuery = NumericRangeQuery.NewInt64Range(
-                valueField,
-                minValue,
-                maxValue,
-                true,
-                true
-            );
-            
-            booleanQuery.Add(optionQuery, Occur.MUST);
-            booleanQuery.Add(rangeQuery, Occur.MUST);
-
-            var topDocs = searcher.Search(booleanQuery, n: 10);
-            
-            Console.WriteLine($"검색 결과 수: {topDocs.TotalHits}");
-            Console.WriteLine("===============================================================================");
-            
-            foreach (var scoreDoc in topDocs.ScoreDocs)
-            {
-                PrintDocumentFields(searcher, scoreDoc);
-            }
-        }
-        finally
-        {
-            _searchManager.Release(searcher);
-            searcher = null;
-            
             stopwatch.Stop();
             Console.WriteLine($"검색 소요 시간: {stopwatch.ElapsedMilliseconds} ms \n");
         }
@@ -269,9 +244,9 @@ public class MySearch
         {
             Console.WriteLine($"- 옵션: {optionName}, 범위: {minValue}~{maxValue}");
         }
-        
+
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-        
+
         _searchManager.MaybeRefreshBlocking();
 
         var searcher = _searchManager.Acquire();
@@ -279,35 +254,32 @@ public class MySearch
         {
             var booleanQuery = new BooleanQuery();
 
+            var num = 1;
             foreach (var (optionName, minValue, maxValue) in options)
             {
-                // 옵션 필드와 값 필드 선택
-                var optionField = $"option{options.IndexOf((optionName, minValue, maxValue)) + 1}";
-                var valueField = $"value{options.IndexOf((optionName, minValue, maxValue)) + 1}";
-
                 // 옵션 이름 쿼리
                 var optionQuery = new WildcardQuery(
-                    new Term(optionField, $"*{optionName.Replace(" ", "")}*")
+                    new Term($"option{num}_value{num}", $"*{optionName.Replace(" ", "")}*") // 첫 번째 옵션 필드 사용
                 );
 
-                // 범위 쿼리
-                var rangeQuery = NumericRangeQuery.NewInt64Range(
-                    valueField,
-                    minValue,
-                    maxValue,
-                    true,
-                    true
-                );
+                // 범위 쿼리 생성
+                var rangeQuery = new BooleanQuery();
+                for (var value = minValue; value <= maxValue; value++)
+                {
+                    rangeQuery.Add(new WildcardQuery(new Term($"option{num}_value{num}", $"*{optionName.Replace(" ", "")}:{value}*")), Occur.SHOULD);
+                }
 
                 booleanQuery.Add(optionQuery, Occur.MUST);
                 booleanQuery.Add(rangeQuery, Occur.MUST);
+
+                num++;
             }
 
             var topDocs = searcher.Search(booleanQuery, n: 10);
-            
+
             Console.WriteLine($"검색 결과 수: {topDocs.TotalHits}");
             Console.WriteLine("===============================================================================");
-            
+
             foreach (var scoreDoc in topDocs.ScoreDocs)
             {
                 PrintDocumentFields(searcher, scoreDoc);
@@ -317,7 +289,7 @@ public class MySearch
         {
             _searchManager.Release(searcher);
             searcher = null;
-            
+
             stopwatch.Stop();
             Console.WriteLine($"검색 소요 시간: {stopwatch.ElapsedMilliseconds} ms \n");
         }
--- lucenenet_study/Program.cs
+++ lucenenet_study/Program.cs
@@ -4,37 +4,79 @@
     {
         private static void Main(string[] args)
         {
-            var search = new MySearch(Path.Combine(Environment.CurrentDirectory, "index"));
-            
-            search.Index();
-            Console.WriteLine($"세팅 종료 \n");
+            /*var search = new MySearch(Path.Combine(Environment.CurrentDirectory, "index"));
 
-            /*search.Search("롱 소");
+            search.Index();
+
+            search.Search("롱 소");
             search.Search("스퉤프");
             search.Search("리공");
             search.Search("리 공");
-            search.Search("물리 공격"); 
+            search.Search("물리 공격");
             search.Search("법 공");
-            
+
             search.Search("힘 증가", "물리 공격력 증가");
             search.Search("힘 증가", "마법 공격력 증가");
-            
+
             search.Search("민첩 증가", "물리 공격력 증가");
             search.Search("민첩 증가", "마법 공격력 증가");
-            
+
             search.Search("지능 증가", "물리 공격력 증가");
             search.Search("지능 증가", "마법 공격력 증가");
-            
-            search.Search("정신력 증가", "물리 공격력 증가");
-            search.Search("정신력 증가", "마법 공격력 증가");*/
 
-            // 옵션1의 수치가 5이상 10이하인 아이템 검색
-            search.SearchByRange(1, 5, 10);
+            search.Search("정신력 증가", "물리 공격력 증가");
+            search.Search("정신력 증가", "마법 공격력 증가");
 
             // 2번째 옵션이 "물리 공격력 증가" 이고 수치가 5이상 10이하인 아이템 검색
             search.SearchByOptionAndRange(2, "물리 공격력 증가", 5, 10);
 
             search.SearchByOptions(new List<(string optionName, int minValue, int maxValue)>
+            {
+                ("힘 증가", 10, 20),        // 첫 번째 옵션: 힘 증가, 범위 10~20
+                ("물리 공격력 증가", 5, 15)  // 두 번째 옵션: 물리 공격력 증가, 범위 5~15
+            });*/
+
+            TempFunc1();
+            
+            Console.WriteLine(" ********************************************************************************** ");
+
+            TempFunc2();
+        }
+        
+        private static void TempFunc1()
+        {
+            var search = new MySearch(Path.Combine(Environment.CurrentDirectory, "index1"));
+
+            search.Index();
+            
+            search.Search("롱 소");
+            search.Search("물리 공격");
+            search.Search("힘 증가", "물리 공격력 증가");
+            
+            // 2번째 옵션이 "물리 공격력 증가" 이고 수치가 5이상 10이하인 아이템 검색
+            search.SearchByOptionAndRange(2, "물리 공격력 증가", 5, 10);
+            
+            search.SearchByOptions(new List<(string optionName, int minValue, int maxValue)>
+            {
+                ("힘 증가", 10, 20),        // 첫 번째 옵션: 힘 증가, 범위 10~20
+                ("물리 공격력 증가", 5, 15)  // 두 번째 옵션: 물리 공격력 증가, 범위 5~15
+            });
+        }
+
+        private static void TempFunc2()
+        {
+            var searchV2 = new MySearchV2(Path.Combine(Environment.CurrentDirectory, "index2"));
+
+            searchV2.Index();
+            
+            searchV2.SearchWithName("롱 소");
+            searchV2.SearchWithOption("물리 공격");
+            searchV2.SearchWithOptions("힘 증가", "물리 공격력 증가");
+            
+            // 2번째 옵션이 "물리 공격력 증가" 이고 수치가 5이상 10이하인 아이템 검색
+            searchV2.SearchByOptionAndRange(2, "물리 공격력 증가", 5, 10);
+            
+            searchV2.SearchByOptions(new List<(string optionName, int minValue, int maxValue)>
             {
                 ("힘 증가", 10, 20),        // 첫 번째 옵션: 힘 증가, 범위 10~20
                 ("물리 공격력 증가", 5, 15)  // 두 번째 옵션: 물리 공격력 증가, 범위 5~15
